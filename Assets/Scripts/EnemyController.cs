e7a0dfcb110d0c66af888a038faf3f298eb9e716
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-08-25 16:58:02 -0300

    Created the project, added the player and the enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Adjust the speed for the application.
+    public float speed = 1.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move our position a step closer to the target.
+        float step = speed * Time.deltaTime; // calculate distance to move
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.target.transform.position, step);
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Swap the position of the cylinder.
+            target.transform.position *= -1.0f;
+        }
+    }
+}
